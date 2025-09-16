e00057125f9e4be4a86788d53be960b3311c8110
Marcos Souzza <marcossouza.git@gmail.com>
2025-09-16 10:56:57 -0300

    Update dos exercícios e criação do diretório Udemy com html

---
+++ UDEMY/AULAS/script.js
@@ -0,0 +1,13 @@
+//Soma dos Numeros
+let num1 = prompt('Digite um número: ');
+num1 = Number(num1); 
+
+let num2 = prompt('Digite outro número: ');
+num2 = Number(num2);
+ 
+const resultado = num1 + num2;
+alert(`O resultado da sua conta foi: ${resultado}`);
+
+
+ 
+
